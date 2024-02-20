c7485d3fffe02ef6b3120b71de1d1826edbb3086
mpmanthan <135797878+mpmanthan@users.noreply.github.com>
2024-02-20 17:45:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,12 @@ resource "aws_security_group" "webserver_access" {
                 protocol = "tcp"
                 cidr_blocks = ["0.0.0.0/0"]
         }
-
+        ingress {
+                from_port = 443
+                to_port = 443
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
         ingress {
                 from_port = 22
                 to_port = 22
