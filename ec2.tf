483ee27a91f22173d2dffb7dd346bc37323cc4ff
mpmanthan <135797878+mpmanthan@users.noreply.github.com>
2024-02-20 17:53:39 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "webserver" {
   ami           = "ami-03f4878755434977f"
   availability_zone = "ap-south-1a"
   instance_type = "t2.micro"
-  key_name = "docker-key"
+  key_name = "full"
   user_data = "${file("install_all.sh")}"
   security_groups = ["${aws_security_group.webserver_access.name}"]
   tags = {
@@ -44,16 +44,5 @@ resource "aws_instance" "webserver" {
 
 }
 
-resource "aws_key_pair" "docker" {
-  key_name   = "docker-key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-resource "local_file" "dockerkey" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "docker_key"
-}
+
 
